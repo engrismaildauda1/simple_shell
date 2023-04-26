9f7a428b9bd7116b22c9baaf4a4bac81b8f54f8c
maidubu <120331703+maidubu@users.noreply.github.com>
2023-04-26 23:11:20 +0100

    Update exits.c

--- exits.c
+++ exits.c
@@ -1,9 +1,9 @@
 #include "shell.h"
 
 /**
- **_strncpy - copies a string
- *@dest: the destination string to be copied to
- *@src: the source string
+ **_strncpy - this copies a string
+ *@dest: is the destination string to be copied to
+ *@src: is the source string
  *@n: the amount of characters to be copied
  *Return: the concatenated string
  */
