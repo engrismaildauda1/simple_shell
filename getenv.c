546f64a156d490ff5c671a2fcb826e49d6ffa023
maidubu <120331703+maidubu@users.noreply.github.com>
2023-04-26 23:13:10 +0100

    Update getenv.c

--- getenv.c
+++ getenv.c
@@ -1,9 +1,9 @@
 #include "shell.h"
 
 /**
- * get_environ - returns the string array copy of our environ
+ * get_environ - this returns the string array copy of our environ
  * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
+ *        constant function prototype.
  * Return: Always 0
  */
 char **get_environ(info_t *info)
